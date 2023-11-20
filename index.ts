071cc3082153fb1cbe5a7e760b7daea05bc2b517
abir14016 <abir14016@gmail.com>
2023-11-20 14:30:57 +0600

    relation among users, posts, profiles and categories tables

--- index.ts
+++ index.ts
@@ -5,15 +5,6 @@ const prisma = new PrismaClient();
 async function main() {
   //   const getAllUsers = await prisma.user.findMany();
   //   console.log(getAllUsers);
-
-  const postUser = await prisma.user.create({
-    data: {
-      email: "abir@gmail.com",
-      name: "Abir",
-      age: 27,
-    },
-  });
-  console.log(postUser);
 }
 
 main();
