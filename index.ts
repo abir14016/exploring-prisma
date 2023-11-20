ebd8dbe85d9510ef958fabdf314eeb175f6eb1a7
abir14016 <abir14016@gmail.com>
2023-11-20 12:50:23 +0600

    exploring migrate and generate

--- index.ts
+++ index.ts
@@ -3,13 +3,14 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const getAllUsers = await prisma.user.findMany();
-  console.log(getAllUsers);
+  //   const getAllUsers = await prisma.user.findMany();
+  //   console.log(getAllUsers);
 
   const postUser = await prisma.user.create({
     data: {
       email: "abir@gmail.com",
       name: "Abir",
+      age: 27,
     },
   });
   console.log(postUser);
